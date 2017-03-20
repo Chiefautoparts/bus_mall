2e80b73c5ffc2da9c65c79c4ac58f6a26e8c3aac
Chiefautoparts <chiefautoparts@outlook.com>
2017-03-20 14:27:42 -0700

    start

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Var imageArr = [];
+
+
+var randomImage = function() {
+  return Math.floor(Math.random() * (20 - 1 + 1) + 1);
+};
+
+var firstChoice = function() {
+  for (var i = 0; i < imageArr.length; i++)
+
+}
